31ad6d92f9e1d1894fc21b998c0fe8db1261aacd
christiangerbig <chr_gerbig@web.de>
2021-01-26 20:56:02 +0100

    mvp

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,3 @@
-
-
-
 function displayMainScreen() {
 
   // Init objects
@@ -36,17 +33,24 @@ function displayMainScreen() {
   }
 
   const flyingLetters = [
+    "HFEODUSRE",
     "DTGCSEAFT",
-    "HFEODUSRE"
+    "MZOIDUEMS"
   ];
 
   const templateWords = [
+    "HOUSE",
     "CAT",
-    "HOUSE"
+    "MOUSE"
   ];
 
   let letters = [];
 
+  let assembledWord = "      ";
+
+  let startIndex = Math.floor(Math.random()*templateWords.length);
+
+  let currentTemplateWord = templateWords[startIndex];
 
    // Init Canvas stuff
   let canvas = document.querySelector("canvas");
@@ -78,22 +82,25 @@ function displayMainScreen() {
 
   // Init objects and arrays
   let pen = new PenObject(10,canvas.height/2,163,16,penImage);
-  let drop = new DropObject(10+pen.width,canvas.height/2,27,19,dropImage);
+  let drop = new DropObject(10+pen.width,canvas.height/2,23,16,dropImage);
 
-  for (let i = 0; i < flyingLetters.length; i++) {
-    for (let j = 0; j < flyingLetters[i].length; j++) {
-      let x = canvas.width - (Math.floor(Math.random()*500)) - 50;
-      let y = 50 + (Math.floor(Math.random()*500));
-      let width = 50;
-      let height = 50;
-      let yDirection = 1 + (Math.floor(Math.random()*3));
-      let letterObject = new LetterObject(x,y,width,height,yDirection,flyingLetters[i][j]);
-      letters.push(letterObject);
-    }
+  let letterXSize = 39;
+  let letterYSize = 57;
+
+  for (let i = 0; i < flyingLetters[startIndex].length; i++) {
+    let x = canvas.width - (Math.floor(Math.random()*500)) - (2*letterXSize);
+    let y = letterYSize + (Math.floor(Math.random()*500));
+    let width = letterXSize;
+    let height = letterYSize;
+    let yDirection = 1 + (Math.floor(Math.random()*2));
+    let letterObject = new LetterObject(x,y,width,height,yDirection,flyingLetters[startIndex][i]);
+    letters.push(letterObject);
   }
+
 
   let dropEnabled = false;
   let intervalId = 0;
+
 
   
   // Add event listener for mouse movement and mouse button
@@ -114,12 +121,14 @@ function displayMainScreen() {
     }
   );
 
-  // Add event listener for esc key
+  // Add event listener for Escape key
   document.addEventListener(
     "keydown",
     function(event) {
-     if ((event.keyCode == 39 || event.key == "ArrowRight")) {
+     if ((event.keyCode == 27 || event.key == "Escape")) {
         clearInterval(intervalId);
+        canvas.classList.remove("cursorOff");
+        canvas.classList.add("cursorOn");
       }
     }
   );
@@ -137,7 +146,7 @@ function displayMainScreen() {
   function shootInk() {
     if (dropEnabled) {
       ctx.drawImage(drop.imageUrl,drop.xPos,drop.yPos);
-      drop.xPos +=5;
+      drop.xPos +=3;
       if (drop.xPos > canvas.width) {
         dropEnabled = false;
         drop.xPos = 10+pen.width;
@@ -145,47 +154,84 @@ function displayMainScreen() {
     }
   }
 
-  function checkLetterHit() {
-    const xCollisionCheck1 = (drop.xPos + drop.width) > letters[i].xPos;
-    const xCollisionCheck2 = (drop.xPos + drop.width) < (letters[i].xPos + letters[i].width);
-    const yCollisionCheck1 = (drop[i].yPos + drop[i].height) > letters[i].height;
-    const yCollisionCheck2 = (drop[i].yPos + drop[i].height) < (letters[i].yPos + letters[i].height);
-    if ((xCollisionCheck1 || xCollisionCheck2) && (yCollisionCheck1 || yCollisionCheck2)) {
+
+  function checkMissingLetter(i) {
+    let buffer = "";
+    for (let j = 0; j < currentTemplateWord.length; j++) {
+      if (letters[i].char == currentTemplateWord[j]) {
+        for (let k = 0; k < assembledWord.length; k++) {
+          let char = assembledWord[k];
+          if (j !== k) {
+            buffer += char;
+          }
+          else {
+            buffer += letters[i].char;
+          }
+        }
+        assembledWord = buffer;
+      }
+    }
+  }
+
+
+  function checkLetterHit(i) {
+    const xCollisionCheck1 = ((drop.xPos + drop.width) >= letters[i].xPos);
+    const yCollisionCheck1 = (drop.yPos >= letters[i].yPos) && (drop.yPos <= (letters[i].yPos + letters[i].height));
+    const yCollisionCheck2 = ((drop.yPos + drop.height) <= (letters[i].ypos + letters[i].height)) && ((drop.yPos + drop.height) >= letters[i].yPos);    
+    if (xCollisionCheck1 && (yCollisionCheck1 || yCollisionCheck2)) {
+      checkMissingLetter(i);
       letters.splice(i,1);
+      dropEnabled = false;
+      drop.xPos = 10+pen.width;
       return true;
     }
+    return false;
   }
 
 
   function moveLetters() {
     ctx.font = "50px Verdana";
     ctx.fillStyle = "yellow";
-    
-
-
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < letters.length; i++) {
       ctx.fillText(letters[i].char,letters[i].xPos,letters[i].yPos);
-      if (checkLetterHit()) {
-        continue;
+      if (dropEnabled) {
+        if (checkLetterHit(i)) {
+          continue;
+        }
       }
-      if ((letters[i].yPos < 50) || (letters[i].yPos > 580)) {
+      if ((letters[i].yPos < letterYSize) || (letters[i].yPos > 580)) {
         letters[i].yDirection *= - 1;
       }
       letters[i].yPos += letters[i].yDirection;
     }
   }
 
+
+  function displayTemplateWord() {
+    ctx.font = "40px Verdana";
+    ctx.fillStyle = "red";
+    ctx.fillText(templateWords[startIndex],10,canvas.height-15);
+  }
+
+  function displayAssembledWord() {
+    ctx.font = "40px Verdana";
+    ctx.fillStyle = "blue";
+    ctx.fillText(assembledWord,600,canvas.height-15);
+  }
+
+
   function animateAll() {
-  
     displayBgPicture();
-    displayPen();
+    displayPen();   
     shootInk();
     moveLetters();
+    displayTemplateWord();
+    displayAssembledWord();
   }
 
 
   // Start interval
-  intervalID = setInterval(
+  intervalId = setInterval(
     function() {
       requestAnimationFrame(animateAll);
     }, 
